90b8dfc68499b97a96cb153a1c0532fbaaf74a15
dkRaptor <dhrtklpsh82@gmail.com>
2015-08-28 13:00:40 +0530

    Updated Commit
    Contact details, plan informations and social links updated.

--- js/plan.js
+++ js/plan.js
@@ -13,7 +13,7 @@ function activateDomesticDialog() {
 
 function activateBuisnessDialog() {
 	vex.dialog.alert({
-		message: "<div id=spacer><h1>Work hassle free with our buisness plans.</h1></div><table><tr><th class=TableHeader colspan=3><b>BUISNESS PLAN</b></th></tr><tr><th class=Header1>PLAN NAME</th><th class=Header2> PLAN TYPE</th><th class=Header3>SPEED</th><th class=Header4> VALIDITY </th><th class=Header5>PRICE(Rs)</th></tr><tr><td>ULTD_384 K</td><td>Unlimited</td><td>384 Kbps</td><td>30 days</td><td>470</td></tr><tr><td>ULTD_576K</td><td>Unlimited</td><td>576 Kbps</td><td>30 days</td><td>650</td></tr><tr><td>ULTD_768K</td><td>Unlimited</td><td>768 Kbps</td><td>30 days</td><td>850</td></tr><tr><td>ULTD_1M</td><td>Unlimited</td><td>1 Mbps</td><td>30 days</td><td>980</td></tr><tr><td>ULTD_2M</td><td>Unlimited</td><td>2 Mbps</td> <td>30 days</td><td>1490</td></tr></table>",
+		message: "<div id=spacer><h1>Work hassle free with our buisness plans.</h1></div><table><tr><th class=TableHeader colspan=3><b>BUISNESS PLAN</b></th></tr><tr><th class=Header1>PLAN NAME</th><th class=Header2> PLAN TYPE</th><th class=Header3>SPEED</th><th class=Header4> VALIDITY </th><th class=Header5>PRICE(Rs)</th></tr><tr><td>B_512K</td><td>Unlimited</td><td>512 Kbps</td><td>30 days</td><td>980</td></tr><tr><td>B_1</td><td>Unlimited</td><td>1 Mbps</td><td>30 days</td><td>1650</td></tr><tr><td>B_2</td><td>Unlimited</td><td>2 Mbps</td><td>30 days</td><td>3100</td></tr><tr><td>B_3</td><td>Unlimited</td><td>3 Mbps</td><td>30 days</td><td>4300</td></tr><tr><td>B_4</td><td>Unlimited</td><td>4 Mbps</td> <td>30 days</td><td>5800</td></tr></table>",
 		className: 'vex-theme-os'
 		
 		 });
@@ -21,7 +21,7 @@ function activateBuisnessDialog() {
 
 function activateCorporateDialog() {
 	vex.dialog.alert({
-		message: "<div id=spacer><h1>Fast paced plans for fast paced corporate world</h1></div><table><tr><th class=TableHeader colspan=3><b>UNLIMITED CORPORATE PLAN</b></th></tr><tr><th class=Header1>PLAN NAME</th><th class=Header2> PLAN TYPE</th><th class=Header3>SPEED</th><th class=Header4> VALIDITY </th><th class=Header5>PRICE(Rs)</th></tr><tr><td>ULTD_384 K</td><td>Unlimited</td><td>384 Kbps</td><td>30 days</td><td>470</td></tr><tr><td>ULTD_576K</td><td>Unlimited</td><td>576 Kbps</td><td>30 days</td><td>650</td></tr><tr><td>ULTD_768K</td><td>Unlimited</td><td>768 Kbps</td><td>30 days</td><td>850</td></tr><tr><td>ULTD_1M</td><td>Unlimited</td><td>1 Mbps</td><td>30 days</td><td>980</td></tr><tr><td>CUSTOMIZED</td><td colspan=4>According to Customer's requirement</td></tr></table>",
+		message: "<div id=spacer><h1>Fast paced plans for fast paced corporate world</h1></div><table><tr><th class=TableHeader colspan=3><b>UNLIMITED CORPORATE PLAN</b></th></tr><tr><th class=Header1>PLAN NAME</th><th class=Header2> PLAN TYPE</th><th class=Header3>SPEED</th><th class=Header4> VALIDITY </th><th class=Header5>PRICE(Rs)</th></tr><tr><td>COR_1</td><td>Unlimited</td><td>1 Mbps</td><td>30 days</td><td>5500</td></tr><tr><td>COR_2</td><td>Unlimited</td><td>2 Mbps</td><td>30 days</td><td>12000</td></tr><tr><td>COR_3</td><td>Unlimited</td><td>3 Mbps</td><td>30 days</td><td>17500</td></tr><tr><td>COR_4</td><td>Unlimited</td><td>4 Mbps</td><td>30 days</td><td>23000</td></tr><tr><td>CUSTOMIZED</td><td colspan=4>According to Customer's requirement</td></tr></table>",
 		className: 'vex-theme-os'
 		
 		 });
